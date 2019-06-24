15f384e0ccd4d31079c81490afdbbbf91b2a64cc
Joaquin Coronado <joaquincora@hotmail.com>
2019-06-24 09:58:10 -0500

    Add first test to web scraping

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hola mundo en GO")
+
+	response, err := http.Get("https://avena.io/panel/#/login");
+
+	if(err != nil){
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	// Copy data from the response to standard output
+	n, err := io.Copy(os.Stdout, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(n)
+
 }
